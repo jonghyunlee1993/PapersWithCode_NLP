591ee3a602972a01c453fe28355977db0778ba32
Jonghyun Lee <green198800@yonsei.ac.kr>
2021-03-30 11:40:49 +0900

    Fix multi 30k import bug

--- 03.seq2seq_attention/seq2seq_with_attention/setup.sh
+++ 03.seq2seq_attention/seq2seq_with_attention/setup.sh
@@ -1,8 +1,12 @@
 pip install -r requirements.txt
 
-echo "Download spacy module ... "
-
 python -m spacy download fr_core_news_sm
 python -m spacy download en_core_web_sm
 
-echo "spacy fr, en core successfully downloaded!"
+git clone https://github.com/multi30k/dataset.git
+
+cp dataset/data/task1/raw/train.fr.gz .data/multi30k
+cp dataset/data/task1/raw/val.fr.gz .data/multi30k
+
+gzip -d .data/multi30k/train.fr.gz
+gzip -d .data/multi30k/val.fr.gz
